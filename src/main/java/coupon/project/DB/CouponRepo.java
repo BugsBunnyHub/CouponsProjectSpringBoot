f1169ea6d28e4f8cc9bf45c53a2bcb592d1eba0c
BugsBunnyHub <sbhtkxpto@gmail.com>
2019-11-18 09:06:13 +0200

    CouponProjectSpring

--- src/main/java/coupon/project/DB/CouponRepo.java
+++ src/main/java/coupon/project/DB/CouponRepo.java
@@ -3,12 +3,10 @@ package coupon.project.DB;
 import coupon.project.beans.Company;
 import coupon.project.beans.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
-@Repository
 public interface CouponRepo extends JpaRepository<Coupon, Integer> {
 
     Coupon findCouponByTitle(String title);
